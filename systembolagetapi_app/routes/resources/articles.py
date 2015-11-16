75b82fa22c4fa54eb15a7dbc0faa154ba35dbe2a
Ludvig Jansson <ludjan@kth.se>
2015-11-16 23:13:53 +0100

    API now retrieves image url and description from systembolaget website

--- systembolagetapi_app/routes/resources/articles.py
+++ systembolagetapi_app/routes/resources/articles.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from flask import jsonify, abort
 from systembolagetapi_app import app, sb_articles
-import bs4 as bs
+from bs4 import BeautifulSoup
+import requests
 
 
 @app.route('/systembolaget/api/articles', methods=['GET'])
@@ -17,7 +18,25 @@ def get_product(article_number):
         matching_articles = [article for article in sb_articles if article['article_number'] == article_number]
         if not matching_articles:
             abort(404)
-    return jsonify(matching_articles[0])
+    matched_article  = matching_articles[0]
+    image_url = None
+    description = None
+    try:
+    	r = requests.get(matched_article['sb_url'])
+    	soup = BeautifulSoup(r.text, 'html.parser')
+    except:
+    	pass
+    else:
+    	# TODO: Check status code maybe, abort on 404, 500, etc?
+    	desc = soup.findAll('p', {'class' : 'description'})
+    	if desc:
+    		description = desc[0].next
+    	img = soup.find(id = 'product-image-carousel')
+    	if img:
+    		image_url = 'http:%s' % img.find('img')['src']
+    matched_article['description'] = description
+    matched_article['image_url'] = image_url
+    return jsonify(matched_article)
 
 @app.route('/systembolaget/api/articles/departments', methods=['GET'])
 def get_department():
