1d2892595346a13d184aed32a636236c5b421ee9
Ludvig Jansson <ludvig.jansson@outlook.com>
2016-08-06 16:20:08 -0400

    App now detects debug better; reads data from disk if so

--- systembolagetapi_app/routes/resources/articles.py
+++ systembolagetapi_app/routes/resources/articles.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import jsonify, abort, request
-from systembolagetapi_app import app, sb_articles, cache
+from systembolagetapi_app import app, cache
 from systembolagetapi_app.config import CACHE_TIMEOUT
 from bs4 import BeautifulSoup
 import requests
@@ -9,16 +9,16 @@ import json
 
 @app.route('/systembolaget/api/articles', methods=['GET'])
 def get_products():
-    return jsonify({'articles': sb_articles})
+    return jsonify({'articles': app.sb_articles})
 
 
 @app.route('/systembolaget/api/articles/<string:article_number>', methods=['GET'])
 @cache.cached(timeout=CACHE_TIMEOUT)
 def get_product(article_number):
-    matching_article = next((article for article in sb_articles if article['article_id'] == article_number), None)
+    matching_article = next((article for article in app.sb_articles if article['article_id'] == article_number), None)
     if not matching_article:
         # Try again with the article number instead of ID
-        matching_article = next((article for article in sb_articles if article['article_number'] == article_number),
+        matching_article = next((article for article in app.sb_articles if article['article_number'] == article_number),
                                 None)
         if not matching_article:
             abort(404)
@@ -46,7 +46,7 @@ def get_product(article_number):
 def get_departments():
     depts_set = set()
     depts = []
-    for article in sb_articles:
+    for article in app.sb_articles:
         if not article['article_department'] in depts_set:
             depts.append('%s, %s' % (article['article_department'], article['name']))
             depts_set.add(article['article_department'])
--- systembolagetapi_app/routes/resources/stores.py
+++ systembolagetapi_app/routes/resources/stores.py
@@ -1,17 +1,17 @@
 from flask import jsonify, abort
-from systembolagetapi_app import app, sb_stores, cache
+from systembolagetapi_app import app, cache
 from systembolagetapi_app.config import CACHE_TIMEOUT
 
 
 @app.route('/systembolaget/api/stores', methods=['GET'])
 def get_stores():
-    return jsonify({'stores': sb_stores})
+    return jsonify({'stores': app.sb_stores})
 
 
 @app.route('/systembolaget/api/stores/<string:store_id>', methods=['GET'])
 @cache.cached(timeout=CACHE_TIMEOUT)
 def get_store(store_id):
-    matching_store = next((store for store in sb_stores if store['store_id'] == store_id), None)
+    matching_store = next((store for store in app.sb_stores if store['store_id'] == store_id), None)
     if not matching_store:
         abort(404)
     return jsonify(matching_store)
