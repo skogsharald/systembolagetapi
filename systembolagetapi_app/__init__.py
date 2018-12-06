beaddfecfc09dd564cd7f9d9059fd5d1c83ddeae
Ludvig Jansson <ludvig.jansson@outlook.com>
2018-12-06 22:38:27 +0100

    CHG: Updated to flask_cache

--- systembolagetapi_app/__init__.py
+++ systembolagetapi_app/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flasgger import Swagger
-from flask.ext.cache import Cache
+from flask_cache import Cache
 
 app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
