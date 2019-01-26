bf73cfeedc0cb16970783a0ebbb0fe74c3a89647
Ludvig Jansson <ludvig.jansson@outlook.com>
2019-01-26 14:31:37 -0800

    FIX: Only application/json for JSON replies

--- systembolagetapi_app/__init__.py
+++ systembolagetapi_app/__init__.py
@@ -45,8 +45,9 @@ from systembolagetapi_app.routes.handlers import errors
 
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Methods', "GET")
-    response.headers.add('Access-Control-Max-Age', 60 * 60 * 24 * 20)
-    response.headers["Content-Type"] = "application/json; charset=utf-8"
+    if 'docs' not in response.base_url:
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Methods', "GET")
+        response.headers.add('Access-Control-Max-Age', 60 * 60 * 24 * 20)
+        response.headers["Content-Type"] = "application/json; charset=utf-8"
     return response
